5848403e9c7a3291f125d230288934dc3b91d5a8
samannrz <saman.noorzadeh@surgar-surgery.com>
2023-10-16 12:53:45 +0200

    update

--- IncisionDataFolderCreation_V2.py
+++ IncisionDataFolderCreation_V2.py
@@ -8,7 +8,7 @@ import cv2
 # batch_num = 3
 dict = {'nicolas.bourdel': 0, 'Jean-Luc.Pouly': 1, 'giuseppe.giacomello': 2, 'filippo.ferrari': 3,
         'Ervin.Kalfa': 4, 'ebbe.thinggaard': 5}
-annotator = 'Ervin.Kalfa'
+annotator = 'ebbe.thinggaard'
 save_image = False
 for batch_num in [21]:
 
