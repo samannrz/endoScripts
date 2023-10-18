6ff9ed1149d66a6c9bcbbef9c2061a0708bf95bd
samannrz <saman.noorzadeh@surgar-surgery.com>
2023-10-18 14:22:20 +0200

    update

---
+++ main_IncicionComparison_V2.py
@@ -0,0 +1,5 @@
+dict = {'nicolas.bourdel': 0, 'Jean-Luc.Pouly': 1, 'giuseppe.giacomello': 2, 'filippo.ferrari': 3,
+        'Ervin.Kallfa': 4, 'ebbe.thinggaard': 5}
+for annotator in list(dict.keys()):
+    with open("IncisionDataFolderCreation_V2.py") as f:
+        exec(f.read())
