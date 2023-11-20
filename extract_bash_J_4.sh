0f29aaff54c4f07af2ab8ad677f6b14e3156c7c0
samannrz <saman.noorzadeh@surgar-surgery.com>
2023-11-20 12:26:33 +0100

    update

---
+++ extract_bash_J_4.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221125_046_VID001.mp4 -ss 00:05:50 -to 00:06:30 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221125_046_VID001_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221125_046_VID002.mp4 -ss 00:07:20 -to 00:07:40 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221125_046_VID002_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221125_046_VID003.mp4 -ss 00:00:40 -to 00:01:00 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221125_046_VID003_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221125_046_VID003.mp4 -ss 00:07:55 -to 00:08:20 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221125_046_VID003_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221125_046_VID003.mp4 -ss 00:09:50 -to 00:10:05 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221125_046_VID003_trim3.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221130_050_VID001.mp4 -ss 00:00:45 -to 00:01:00 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221130_050_VID001_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221130_050_VID001.mp4 -ss 00:03:00 -to 00:03:10 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221130_050_VID001_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221130_050_VID001.mp4 -ss 00:03:20 -to 00:03:40 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221130_050_VID001_trim3.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221130_050_VID001.mp4 -ss 00:04:10 -to 00:04:30 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221130_050_VID001_trim4.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221130_051_VID002.mp4 -ss 00:03:35 -to 00:03:45 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221130_051_VID002_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221130_051_VID002.mp4 -ss 00:04:00 -to 00:04:30 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221130_051_VID002_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221130_051_VID003.mp4 -ss 00:02:45 -to 00:02:50 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221130_051_VID003_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221130_051_VID003.mp4 -ss 00:03:10 -to 00:03:15 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221130_051_VID003_trim2.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221201_052_VID003.mp4 -ss 00:04:45 -to 00:05:15 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221201_052_VID003_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221201_052_VID003.mp4 -ss 00:05:25 -to 00:05:40 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221201_052_VID003_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221201_052_VID004.mp4 -ss 00:00:10 -to 00:00:45 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221201_052_VID004_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20221201_052_VID004.mp4 -ss 00:03:20 -to 00:03:50 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20221201_052_VID004_trim2.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230120_053_VID002.mp4 -ss 00:02:35 -to 00:02:45 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230120_053_VID002_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230120_053_VID002.mp4 -ss 00:04:23 -to 00:41:50 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230120_053_VID002_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230120_053_VID002.mp4 -ss 00:09:00 -to 00:09:40 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230120_053_VID002_trim3.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230120_053_VID007.mp4 -ss 00:05:10 -to 00:05:30 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230120_053_VID007_trim1.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230124_054_VID001.mp4 -ss 00:04:45 -to 00:05:05 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230124_054_VID001_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230124_054_VID001.mp4 -ss 00:05:20 -to 00:05:30 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230124_054_VID001_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230124_054_VID002.mp4 -ss 00:08:25 -to 00:08:35 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230124_054_VID002_trim1.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230125_056_VID001.mp4 -ss 00:05:15 -to 00:06:00 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230125_056_VID001_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230125_056_VID002.mp4 -ss 00:03:00 -to 00:03:30 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230125_056_VID002_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230125_056_VID007.mp4 -ss 00:05:18 -to 00:05:22 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230125_056_VID007_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230125_056_VID008.mp4 -ss 00:03:20 -to 00:04:00 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230125_056_VID008_trim1.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_057_VID002.mp4 -ss 00:03:30 -to 00:03:40 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_057_VID002_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_057_VID002.mp4 -ss 00:08:40 -to 00:08:52 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_057_VID002_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_057_VID002.mp4 -ss 00:09:10 -to 00:09:10 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_057_VID002_trim3.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_057_VID002.mp4 -ss 00:09:30 -to 00:09:40 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_057_VID002_trim4.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_057_VID002.mp4 -ss 00:10:30 -to 00:10:45 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_057_VID002_trim5.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_057_VID003.mp4 -ss 00:03:25 -to 00:03:40 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_057_VID003_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_057_VID008.mp4 -ss 00:05:25 -to 00:05:45 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_057_VID008_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_057_VID009.mp4 -ss 00:00:10 -to 00:00:30 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_057_VID009_trim1.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_058_VID001.mp4 -ss 00:01:10 -to 00:01:25 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_058_VID001_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_058_VID001.mp4 -ss 00:02:50 -to 00:03:10 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_058_VID001_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230126_058_VID001.mp4 -ss 00:05:35 -to 00:05:45 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230126_058_VID001_trim3.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230127_059_VID001.mp4 -ss 00:07:30 -to 00:08:00 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230127_059_VID001_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230127_059_VID001.mp4 -ss 00:09:08 -to 00:09:15 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230127_059_VID001_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230127_059_VID001.mp4 -ss 00:09:40 -to 00:09:50 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230127_059_VID001_trim3.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230127_059_VID002.mp4 -ss 00:00:00 -to 00:00:10 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230127_059_VID002_trim1.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230130_060_VID001.mp4 -ss 00:04:25 -to 00:04:35 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230130_060_VID001_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230130_060_VID001.mp4 -ss 00:06:55 -to 00:07:15 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230130_060_VID001_trim2.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230130_061_VID001.mp4 -ss 00:04:45 -to 00:05:25 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230130_061_VID001_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230130_061_VID001.mp4 -ss 00:06:30 -to 00:06:45 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230130_061_VID001_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230130_061_VID001.mp4 -ss 00:07:30 -to 00:07:40 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230130_061_VID001_trim3.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230130_061_VID002.mp4 -ss 00:04:00 -to 00:04:20 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230130_061_VID002_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230130_061_VID003.mp4 -ss 00:00:01 -to 00:00:10 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230130_061_VID003_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230130_061_VID004.mp4 -ss 00:05:23 -to 00:05:35 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230130_061_VID004_trim1.mp4
+
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230203_064_VID001.mp4 -ss 00:03:00 -to 00:03:10 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230203_064_VID001_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230203_064_VID002.mp4 -ss 00:02:45 -to 00:03:20 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230203_064_VID002_trim1.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230203_064_VID002.mp4 -ss 00:07:00 -to 00:07:10 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230203_064_VID002_trim2.mp4
+ffmpeg -i /data/Videos/endodata/orig/FCF1_GY_20230203_064_VID004.mp4 -ss 00:01:55 -to 00:02:02 -c:v copy /data/Videos/endodata/sequ/FCF1_GY_20230203_064_VID004_trim1.mp4
