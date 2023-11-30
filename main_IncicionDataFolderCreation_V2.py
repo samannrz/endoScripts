51022f6efe6cd139a88f16b4083f59eed734954d
saman <saman.noorzadeh@surgar-surgery.com>
2023-11-30 14:01:07 +0100

    update

--- main_IncicionDataFolderCreation_V2.py
+++ main_IncicionDataFolderCreation_V2.py
@@ -1,5 +1,5 @@
 dict = {'nicolas.bourdel': 0, 'Jean-Luc.Pouly': 1, 'giuseppe.giacomello': 2, 'filippo.ferrari': 3
-    , 'Ervin.Kallfa': 4} # 'ebbe.thinggaard': 5 , 'incision.consensus': 4
+    , 'Ervin.Kallfa': 4, 'ebbe.thinggaard': 5} # , 'incision.consensus': 4
 for annotator in list(dict.keys()):
     with open("IncisionDataFolderCreation_V2.py") as f:
         exec(f.read())
