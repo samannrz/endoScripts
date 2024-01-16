a3aba458a97103d6f655db8eaa2f1609be33e826
samannrz <saman.noorzadeh@surgar-surgery.com>
2024-01-16 20:10:08 +0100

    update

--- merge_junior_annotations.py
+++ merge_junior_annotations.py
@@ -36,49 +36,49 @@ images1 = os.listdir(os.path.join(orig_path1, 'image/'))
 images2 = os.listdir(os.path.join(orig_path2, 'image/'))
 images_intersect = list(set(images1).intersection(images2))
 
-# for name in images_intersect:
-#     print(name)
-#     orig_image = cv2.imread(os.path.join(orig_path_image1, name))
-#     height, width, _ = orig_image.shape
-#     if width == 853:
-#         width = 854
-#     if os.path.exists(os.path.join(path_mask_treat1, name)):
-#         t1 = cv2.imread(os.path.join(path_mask_treat1, name))
-#     else:
-#         t1 = np.zeros((height, width, 3), dtype=np.uint8)
-#
-#     if os.path.exists(os.path.join(path_mask_treat2, name)):
-#         t2 = cv2.imread(os.path.join(path_mask_treat2, name))
-#     else:
-#         t2 = np.zeros((height, width, 3), dtype=np.uint8)
-#
-#     if os.path.exists(os.path.join(path_mask_check1, name)):
-#         c1 = cv2.imread(os.path.join(path_mask_check1, name))
-#     else:
-#         c1 = np.zeros((height, width, 3), dtype=np.uint8)
-#
-#     if os.path.exists(os.path.join(path_mask_check2, name)):
-#         c2 = cv2.imread(os.path.join(path_mask_check2, name))
-#     else:
-#         c2 = np.zeros((height, width, 3), dtype=np.uint8)
-#
-#     mask_treat = cv2.bitwise_and(t1, t2)
-#
-#     mask_check = cv2.bitwise_or(cv2.bitwise_or(t1, t2), cv2.bitwise_or(t1, c2))
-#     mask_check = cv2.bitwise_or(mask_check, cv2.bitwise_or(t2, c1))
-#     mask_check = cv2.bitwise_or(mask_check, cv2.bitwise_or(c2, c1))
-#
-#     cv2.imwrite(os.path.join(savepath, 'mask/Treat', name), mask_treat)
-#     cv2.imwrite(os.path.join(savepath, 'mask/Check', name), mask_check)
-#     shutil.copy(os.path.join(orig_path_image1, name), os.path.join(savepath, 'image', name))
+for name in images_intersect:
+    print(name)
+    orig_image = cv2.imread(os.path.join(orig_path_image1, name))
+    height, width, _ = orig_image.shape
+    if width == 853:
+        width = 854
+    if os.path.exists(os.path.join(path_mask_treat1, name)):
+        t1 = cv2.imread(os.path.join(path_mask_treat1, name))
+    else:
+        t1 = np.zeros((height, width, 3), dtype=np.uint8)
 
-from functions import find_missing_images
-# Call the function to find and copy missing images
-find_missing_images(orig_path_image1, os.path.join(savepath, 'image'))
-find_missing_images(orig_path_image2, os.path.join(savepath, 'image'))
+    if os.path.exists(os.path.join(path_mask_treat2, name)):
+        t2 = cv2.imread(os.path.join(path_mask_treat2, name))
+    else:
+        t2 = np.zeros((height, width, 3), dtype=np.uint8)
 
-find_missing_images(path_mask_treat1, os.path.join(savepath, 'mask/Treat'))
-find_missing_images(path_mask_treat2, os.path.join(savepath, 'mask/Treat'))
+    if os.path.exists(os.path.join(path_mask_check1, name)):
+        c1 = cv2.imread(os.path.join(path_mask_check1, name))
+    else:
+        c1 = np.zeros((height, width, 3), dtype=np.uint8)
 
-find_missing_images(path_mask_check1, os.path.join(savepath, 'mask/Check'))
-find_missing_images(path_mask_check2, os.path.join(savepath, 'mask/Check'))
+    if os.path.exists(os.path.join(path_mask_check2, name)):
+        c2 = cv2.imread(os.path.join(path_mask_check2, name))
+    else:
+        c2 = np.zeros((height, width, 3), dtype=np.uint8)
+
+    mask_treat = cv2.bitwise_and(t1, t2)
+
+    mask_check = cv2.bitwise_or(cv2.bitwise_or(t1, t2), cv2.bitwise_or(t1, c2))
+    mask_check = cv2.bitwise_or(mask_check, cv2.bitwise_or(t2, c1))
+    mask_check = cv2.bitwise_or(mask_check, cv2.bitwise_or(c2, c1))
+
+    cv2.imwrite(os.path.join(savepath, 'mask/Treat', name), mask_treat)
+    cv2.imwrite(os.path.join(savepath, 'mask/Check', name), mask_check)
+    shutil.copy(os.path.join(orig_path_image1, name), os.path.join(savepath, 'image', name))
+
+# from functions import find_missing_images
+# # Call the function to find and copy missing images
+# find_missing_images(orig_path_image1, os.path.join(savepath, 'image'))
+# find_missing_images(orig_path_image2, os.path.join(savepath, 'image'))
+#
+# find_missing_images(path_mask_treat1, os.path.join(savepath, 'mask/Treat'))
+# find_missing_images(path_mask_treat2, os.path.join(savepath, 'mask/Treat'))
+#
+# find_missing_images(path_mask_check1, os.path.join(savepath, 'mask/Check'))
+# find_missing_images(path_mask_check2, os.path.join(savepath, 'mask/Check'))
