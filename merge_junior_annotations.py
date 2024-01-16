5f32b3bd7cfc3ec8d50a93b859b39b74fe1f49e0
samannrz <saman.noorzadeh@surgar-surgery.com>
2024-01-16 20:18:03 +0100

    update

--- merge_junior_annotations.py
+++ merge_junior_annotations.py
@@ -36,7 +36,7 @@ images1 = os.listdir(os.path.join(orig_path1, 'image/'))
 images2 = os.listdir(os.path.join(orig_path2, 'image/'))
 images_intersect = list(set(images1).intersection(images2))
 
-images3 = os.listdir(os.path.join(base_path, str(4)))
+images3 = os.listdir(os.path.join(base_path, str(4),'images'))
 images_intersect = set(images1).intersection(set(images2)) - set(images3)
 # Convert the resulting set to a list, if needed
 images_intersect_list = list(images_intersect)
