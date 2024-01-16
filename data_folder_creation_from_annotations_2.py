d1e42f7726a1f416e05cb2ac6513d0cee1c1342f
samannrz <saman.noorzadeh@surgar-surgery.com>
2024-01-16 12:21:21 +0100

    update

--- data_folder_creation_from_annotations_2.py
+++ data_folder_creation_from_annotations_2.py
@@ -9,7 +9,7 @@ ws = api.workspace.get_info_by_name(tm.id, 'Data annotation')
 
 ANNOTATOR_DICT = {'nicolas.bourdel': 0, 'Jean-Luc.Pouly': 1, 'giuseppe.giacomello': 2, 'filippo.ferrari': 3,
                   'incision.consensus': 4}
-ANNOTATOR = 0
+ANNOTATOR = 1
 print(ANNOTATOR)
 dest_path = '/data/DATA/incision/'
 # dest_path = '/Users/saman/Documents/data/DATA/incision/'
