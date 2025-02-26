a891a34195afe157ae3d07cab28890bbee0dcaa0
saman <saman.noorzadeh@surgar-surgery.com>
2025-02-26 10:28:29 +0100

    update

--- folders_to_folder.py
+++ folders_to_folder.py
@@ -1,3 +1,4 @@
+# The images in folder1, if are also existing in folder 2, are copied to a new folder
 import os
 import shutil
 
@@ -6,25 +7,25 @@ def check_and_move_files(folder1, folder2, new_folder):
     if not os.path.exists(new_folder):
         os.makedirs(new_folder)
 
-    # Get the list of files in folder2
     folder2_files = os.listdir(folder2)
-
-    # Iterate through files in folder1
+    count = 1
     for file1 in os.listdir(folder1):
-        # Check if the file exists in folder2
         if file1 in folder2_files:
-            # Move the file to the new folder
-            shutil.copy(os.path.join(folder1, file1), os.path.join(new_folder, file1))
-            print(os.path.join(new_folder[:-6],'mask/Treat', file1))
-            shutil.copy(os.path.join(folder1[:-6],'mask/Treat', file1), os.path.join(new_folder[:-6],'mask/Treat', file1))
-            shutil.copy(os.path.join(folder1[:-6], 'mask/Check', file1),
-                        os.path.join(new_folder[:-6], 'mask/Check', file1))
+            if not file1 in os.listdir(new_folder):
+                # Move the file to the new folder
+                shutil.copy(os.path.join(folder1, file1), os.path.join(new_folder, file1))
+                print(os.path.join(new_folder[:-6],'mask/Treat', file1))
+                shutil.copy(os.path.join(folder1[:-6],'mask/Treat', file1), os.path.join(new_folder[:-6],'mask/Treat', file1))
+                shutil.copy(os.path.join(folder1[:-6], 'mask/Check', file1),
+                            os.path.join(new_folder[:-6], 'mask/Check', file1))
+                print(count)
+                count +=1
 
 
 # Paths to the folders
-folder1 = "/data/D"
-folder2 = "/data/projects/IncisionDeepLab/input/incision/orig_data4/test_images"
-new_folder = "/data/DATA/MCTEST"
+folder1 = "/data/DATA/orig_data_expert_consensus/image"
+folder2 = "/data/projects/IncisionDeepLab/input/incision/orig_data4/train_images"
+new_folder = "/data/DATA/orig_data_junior_consensus/image"
 
 # Call the function
 check_and_move_files(folder1, folder2, new_folder)
